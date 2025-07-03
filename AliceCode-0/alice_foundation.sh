905bacade5e498772c7c9836e7c7b36c4cc25bf7
Hana FEKI <hfeki@node121.cluster-fon.interne.mines-paristech.fr>
2025-07-03 17:16:06 +0200

    Ajout du code v0

---
+++ AliceCode-0/alice_foundation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=Foundation                           # Custom job name
+#SBATCH --mem=20000                                     # Memory in MB (20GB)
+#SBATCH --gres=gpu:A40:1                               # GPU type and number
+#SBATCH -p cbio-gpu                                     # Partition (queue)
+#SBATCH --exclude=node005,node009                       # Nodes to avoid
+#SBATCH --cpus-per-task=4                               # Number of CPUs per task
+#SBATCH --output=output_alice_FM.out                     
+
+source /cluster/CBIO/home/hfeki/miniconda3/etc/profile.d/conda.sh
+conda activate hfeki
+
+python train.py 
